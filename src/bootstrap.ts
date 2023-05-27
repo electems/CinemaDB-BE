fecc0be110a18f29172d3c2ccef988922f7fa593
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2023-05-27 18:49:24 +0530

    removed local host and added for all ip address

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -27,7 +27,7 @@ export const bootstrap = async (): Promise<NestExpressApplication> => {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger', app, document);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
